8f5c8fc2cd27d437e7e52040a698beea4207925e
Beni-higher <dikitelebeni@gmail.com>
2024-01-30 00:51:48 -0500

    Mise a jour de la structure du projet en different directory add and ShoppingList.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,17 @@
+import '../styles/Cart.css'
+
+
 function Cart(){
     const monsteraPrice = 8
     const ivyPrice = 10
     const flowerPrice = 15
     return (
-        <div>
+        <div className="lmj-cart">
             <h2>Panier</h2>
             <ul>
-                <li>Monstera : {monsteraPrice}CAD</li>
-                <li>Lierre : {ivyPrice}CAD</li>
-                <li>Fleurs : {flowerPrice}CAD</li>
+                <li>Monstera : {monsteraPrice} CAD</li>
+                <li>Lierre : {ivyPrice} CAD</li>
+                <li>Fleurs : {flowerPrice} CAD</li>
             </ul>
             Total : {monsteraPrice + ivyPrice + flowerPrice} CAD
         </div>
